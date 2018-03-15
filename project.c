2ae91fb4fa60794b5992c22814bb3214244ed55c
sagar-bindal <sagarbindal992@gmail.com>
2018-03-15 19:20:31 +0530

    making of the process table

--- project.c
+++ project.c
@@ -1,8 +1,26 @@
 #include<stdio.h>
+#include<unistd.h>
+struct process
+{
+    int pid;
+    float AT;
+    float BT;
+    float WT;
+    float priority;
+};
 int main()
 {
-    //abcd
-    //starting of linux project
-    int b=50;
-    int a;
+    printf("\n ************************* SHORTEST JOB SCHEDULING ALLGORITHM PROJECT ***********************************");
+    printf("\n\n\n enter the number of processes you want to schedule :  ");
+    int number;
+    scanf("%d",&number);
+    struct process proc[number];
+    
+    printf("\n enter the burst time of the processes \n");
+ 
+    for(int i=0;i<number;i++)
+    {
+        printf("\n enter the burst time for the %d process  :  ",i);
+        scanf("%f",&proc[i].BT);
+    }
 }
