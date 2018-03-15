c0c99bf231872b213f282a3a0aaacf4fad902468
sagar-bindal <sagarbindal992@gmail.com>
2018-03-15 19:40:00 +0530

    making of table

--- project.c
+++ project.c
@@ -1,5 +1,7 @@
 #include<stdio.h>
 #include<unistd.h>
+#include<stdlib.h>
+#include<sys/queue.h>
 struct process
 {
     int pid;
@@ -20,7 +22,33 @@ int main()
  
     for(int i=0;i<number;i++)
     {
-        printf("\n enter the burst time for the %d process  :  ",i);
+        printf("\n enter the burst time for the %d process  :  ",i+1);
         scanf("%f",&proc[i].BT);
+        proc[i].pid=i+1;
     }
+
+    printf("\n enter the arival time of the processes \n");
+    for(int i=0;i<number;i++)
+    {
+        printf("\n enter the arival time for the %d process  :  ",i+1);
+        scanf("%f",&proc[i].AT);
+    }
+
+        printf("\n Process:     |   Burst Time:     |   Arival Time:     | ");
+    for(int i=0;i<number;i++)
+    {
+        printf("\n   %d               %d                   %d      ",proc[i].pid,(int )proc[i].BT,(int )proc[i].AT);
+    }
+    printf("\n");
+    for(int i=0;i<number;i++)
+    {
+        proc[i].WT=0;
+    }
+    
+    int timer=0;
+    
+
+
+
+
 }
